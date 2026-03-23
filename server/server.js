60332d8deb73be09907ab4e85c84acfd14ec66e8
menso431 <menson.sundash@gmail.com>
2026-03-23 23:50:08 +1100

    add route main path to server

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
     res.status(200).json({message: 'JetStock backend is running'})
 })
 
+//main api routes (path to all routes)
+app.use("/api", require("./routes"));
+
 //server is listening port
 app.listen(PORT, () => {
     console.log(`App is listening on http:localhost: ${PORT}`);
