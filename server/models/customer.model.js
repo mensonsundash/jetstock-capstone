d2b26395bb4381cb42a20813afeea1908fcd77c9
menso431 <menson.sundash@gmail.com>
2026-03-23 23:37:35 +1100

    created customer model

---
+++ server/models/customer.model.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db.config");
+
+const Customer = sequelize.define( "customer",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    full_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "customers",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
+
+module.exports = Customer;
