ff9a4ce92fc4a9d86aa1f3edb0b6a03d1c35640d
menso431 <menson.sundash@gmail.com>
2026-03-23 21:53:58 +1100

    add api health check route

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,10 @@ const PORT = 3000;
 // parse requests of content-type = application/json
 app.use(express.json());
 
-
+//testing api route health
+app.get("/", (req, res) => {
+    res.status(200).json({message: 'JetStock backend is running'})
+})
 
 //server is litening port
 app.listen(PORT, () => {
