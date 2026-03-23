8684cd120bf60cc3f3f0fbeca063ec278ab71cfb
menso431 <menson.sundash@gmail.com>
2026-03-23 23:19:22 +1100

    created inventory model

---
+++ server/models/inventory.model.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db.config");
+
+// mysql schema generator to create table
+const Inventory = sequelize.define( "inventory",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    quantity_on_hand: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    reorder_level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 5,
+    },
+    location: {
+      type: DataTypes.STRING(120),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "inventory",
+    timestamps: false,
+    updatedAt: "updated_at",
+  }
+);
+
+module.exports = Inventory;
