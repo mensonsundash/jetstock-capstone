88059d7e5cbf7ba77473c1cf4dbd7fc4453dca26
menso431 <menson.sundash@gmail.com>
2026-03-23 23:11:03 +1100

    created supplier model

---
+++ server/models/supplier.model.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+const { Sequelize } = require("../config/db.config");
+
+// mysql schema generator to create table
+const Supplier = sequelize.define('supplier', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+    },
+    contact_person: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+},{
+    tableName: 'suppliers',
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: 'updated_at'
+});
+
+module.exports = Supplier;
