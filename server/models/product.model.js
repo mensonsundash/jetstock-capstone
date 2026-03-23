c09f1af429620191a461563e012f10e0533a9b2a
menso431 <menson.sundash@gmail.com>
2026-03-23 23:16:42 +1100

    created product  model

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,8 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db.config");
 
-// mysql schema generator to create table
-const Category = sequelize.define("category",
+const Product = sequelize.define( "product",
   {
     id: {
       type: DataTypes.INTEGER,
@@ -10,25 +9,46 @@ const Category = sequelize.define("category",
       autoIncrement: true,
       allowNull: false,
     },
+    supplier_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-    name: {
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sku: {
       type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING(120),
       allowNull: false,
     },
     description: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
   },
   {
-    tableName: "categories",
+    tableName: "products",
     timestamps: true,
     createdAt: "created_at",
     updatedAt: "updated_at",
   }
 );
 
-module.exports = Category;
+module.exports = Product;
