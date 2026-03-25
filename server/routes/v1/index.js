c57a2ac07f14218f4f990727860eadfdd019daa7
menso431 <menson.sundash@gmail.com>
2026-03-25 22:12:06 +1100

    creating sub routes torouters

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -7,6 +7,12 @@ app.get("/", (req,res) => {
 
 
 // sub router of server
-
-
+app.use("/users", require("./user.routes")); // router to user routes
+app.use("/suppliers", require("./supplier.routes")); // router to user routes
+app.use("/categories", require("./category.routes")); // router to user routes
+app.use("/products", require("./product.routes")); // router to user routes
+app.use("/inventory", require("./inventory.routes")); // router to user routes
+app.use("/stock-movements", require("./stockMovement.routes")); // router to user routes
+app.use("/customers", require("./customer.routes")); // router to user routes
+app.use("/orders", require("./order.routes")); // router to user routes
 module.exports = app;
