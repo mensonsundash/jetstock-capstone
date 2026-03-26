8aea540e3ff3b76827b027729edf9ea1edb7f267
menso431 <menson.sundash@gmail.com>
2026-03-26 22:05:24 +1100

    separating & validating authenticate user and its role

--- server/middlewares/role.middleware.js
+++ server/middlewares/role.middleware.js
@@ -2,12 +2,26 @@
 // authorizeRoles("admin") // using spread operator to accept any number of parameters
 const authorizeRoles = (...allowedRoles) => {
     return (req,res, next) => {
-        // validating req.user which is already authenticated and provided from authenticate middleware to be used
-        if(!req.user || !allowedRoles.includes(req.user.role)) {
-            return res.status(403).json({ message: "Forbidden: you don't have permission to access this module" });
+
+        try{
+            // check if user exists which is provided by authenticate middleware
+            if(!req.user) {
+                //user correct but not authorized access
+                return res.status(401).json({ message: "Unauthorized access" });    
+            }   
+
+            // validating req.user which is already authenticated and provided from authenticate middleware to be used
+            if(!allowedRoles.includes(req.user.role)) {
+                return res.status(403).json({ message: "Forbidden: you don't have permission to access this module" });
+            }
+
+            next(); // if user has correct role and pass this perimeter then continue request to next
+
+        } catch(error) {
+            return res.status(500).json({message: 'Authorization failed', error: error.message});
         }
 
-        next(); // if user has correct role and pass this perimeter then continue request to next
+        
     };
 };
 
