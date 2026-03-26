5e60228f814a7e4713e429b1c574db8a28b5bdee
menso431 <menson.sundash@gmail.com>
2026-03-26 22:06:35 +1100

    JWT & role protection on user routes

--- server/routes/v1/user.routes.js
+++ server/routes/v1/user.routes.js
@@ -4,11 +4,15 @@ const app = express.Router();
 //importing user controller
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require("../../controllers/user.controller");
 
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
+
 //CRUD routers connecting its controllers
-app.get("/", getAllUsers);
-app.get("/:id", getUserById);
-app.post("/", createUser);
-app.put("/:id", updateUser);
-app.delete("/:id", deleteUser);
+app.get("/", authenticate, authorizeRoles("admin"), getAllUsers); // Protected: only admin can view all user
+app.get("/:id", authenticate, authorizeRoles("admin"), getUserById); // Protected: only admin can view all user
+app.post("/", createUser);// Public: as user can register
+app.put("/:id", authenticate, authorizeRoles("admin", "user"), updateUser); // Protected: loggedin user and admin can update
+app.delete("/:id", authenticate, authorizeRoles("admin", "user"), deleteUser); // Protected: only admin can view all user
 
 module.exports = app;
