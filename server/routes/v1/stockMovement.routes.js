092b0969aefdcb200c4f601a0a1d33a6c6fbbd0e
menso431 <menson.sundash@gmail.com>
2026-03-26 22:24:30 +1100

    JWT & role protection on stockMovements routes

--- server/routes/v1/stockMovement.routes.js
+++ server/routes/v1/stockMovement.routes.js
@@ -8,13 +8,17 @@ const { getAllStockMovements,
     createStockMovement,
     stockInProduct,
     stockOutProduct} = require("../../controllers/stockMovement.controller");
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
 
 //CRUD routers connecting its controllers
-app.get("/", getAllStockMovements);
-app.get("/:id", getStockMovementById);
-app.get("/product/:productId", getStockMovementsByProductId);
-app.post("/", createStockMovement);
-app.post("/stock-in", stockInProduct);
-app.post("/stock-out", stockOutProduct);
+// Protected: loggedin user and admin can get/post/put/delete operations
+app.get("/", authenticate, authorizeRoles("admin", "user"), getAllStockMovements);
+app.get("/:id", authenticate, authorizeRoles("admin", "user"), getStockMovementById);
+app.get("/product/:productId", authenticate, authorizeRoles("admin", "user"), getStockMovementsByProductId);
+app.post("/", authenticate, authorizeRoles("admin", "user"), createStockMovement);
+app.post("/stock-in", authenticate, authorizeRoles("admin", "user"), stockInProduct);
+app.post("/stock-out", authenticate, authorizeRoles("admin", "user"), stockOutProduct);
 
 module.exports = app;
