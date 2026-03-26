137810f858a81a27b4de62821a9b82ab49287b42
menso431 <menson.sundash@gmail.com>
2026-03-26 22:12:22 +1100

    JWT & role protection on category routes

--- server/routes/v1/category.routes.js
+++ server/routes/v1/category.routes.js
@@ -4,11 +4,20 @@ const app = express.Router();
 //importing category controller
 const { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory} = require("../../controllers/category.controller")
 
+
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
+
 //CRUD routers connecting its controllers
-app.get("/", getAllCategories);
-app.get("/:id", getCategoryById);
-app.post("/", createCategory);
-app.put("/:id", updateCategory);
-app.delete("/:id", deleteCategory);
+
+// Protected: loggedin user and admin can view categories
+app.get("/", authenticate, authorizeRoles("admin", "user"), getAllCategories);
+app.get("/:id", authenticate, authorizeRoles("admin", "user"), getCategoryById);
+
+app.post("/", authenticate, authorizeRoles("admin", "user"), createCategory); // Protected: loggedin user and admin can create
+app.put("/:id", authenticate, authorizeRoles("admin", "user"), updateCategory);// Protected: loggedin user and admin can update
+app.delete("/:id", authenticate, authorizeRoles("admin", "user"), deleteCategory);// Protected: loggedin user and admin can delete
 
 module.exports = app;
+
