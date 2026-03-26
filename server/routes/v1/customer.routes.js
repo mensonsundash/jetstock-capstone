749cc16bd788cb6e517b6aa9b3ec068e767c9ce5
menso431 <menson.sundash@gmail.com>
2026-03-26 22:26:21 +1100

    JWT & role protection on customer routes

--- server/routes/v1/customer.routes.js
+++ server/routes/v1/customer.routes.js
@@ -8,11 +8,17 @@ const { getAllCustomers,
     updateCustomer,
     deleteCustomer} = require("../../controllers/customer.controller");
 
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
+
 //CRUD routers connecting its controllers
-app.get("/", getAllCustomers);
-app.get("/:id", getCustomerById);
-app.post("/", createCustomer);
-app.put("/:id", updateCustomer);
-app.delete("/:id", deleteCustomer);
+// Protected: loggedin user and admin can get/post/put/delete operations
+app.get("/", authenticate, authorizeRoles("admin", "user"), getAllCustomers);
+app.get("/:id", authenticate, authorizeRoles("admin", "user"), getCustomerById);
+app.post("/", authenticate, authorizeRoles("admin", "user"), createCustomer);
+app.put("/:id", authenticate, authorizeRoles("admin", "user"), updateCustomer);
+//For now only admin can delete the customers
+app.delete("/:id", authenticate, authorizeRoles("admin"), deleteCustomer);
 
 module.exports = app;
