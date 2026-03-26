4b1fc86b42bead80b5d3576658698b595acf0530
menso431 <menson.sundash@gmail.com>
2026-03-26 22:27:31 +1100

    JWT & role protection on orders routes

--- server/routes/v1/order.routes.js
+++ server/routes/v1/order.routes.js
@@ -8,11 +8,17 @@ const { getAllOrders,
     updateOrderStatus,
     deleteOrder} = require("../../controllers/order.controller");
 
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
+
 //CRUD routers connecting its controllers
-app.get("/", getAllOrders);
-app.get("/:id", getOrderById);
-app.post("/", createOrder);
-app.put("/:id", updateOrderStatus);
-app.delete("/:id", deleteOrder);
+// Protected: loggedin user and admin can get/post/put/delete operations
+app.get("/", authenticate, authorizeRoles("admin", "user"), getAllOrders);
+app.get("/:id", authenticate, authorizeRoles("admin", "user"), getOrderById);
+app.post("/", authenticate, authorizeRoles("admin", "user"), createOrder);
+app.put("/:id", authenticate, authorizeRoles("admin", "user"), updateOrderStatus);
+//for now only admin can delete orders
+app.delete("/:id", authenticate, authorizeRoles("admin"), deleteOrder);
 
 module.exports = app;
