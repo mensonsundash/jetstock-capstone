117ab429cb5a1e8aef5892958a1df40656602ba8
menso431 <menson.sundash@gmail.com>
2026-03-26 22:10:39 +1100

    JWT & role protection on supplier routes

--- server/routes/v1/supplier.routes.js
+++ server/routes/v1/supplier.routes.js
@@ -4,11 +4,17 @@ const app = express.Router();
 //importing supplier controller
 const { getAllSuppliers, getSupplierById, createSupplier, updateSupplier, deleteSupplier} = require("../../controllers/supplier.controller");
 
+// importing middleware that verifies user token and roles
+const authenticate = require("../../middlewares/auth.middleware"); // importing auth middleware
+const authorizeRoles = require("../../middlewares/role.middleware"); // importing role middleware
+
 //CRUD routers connecting its controllers
-app.get("/", getAllSuppliers);
-app.get("/:id", getSupplierById);
-app.post("/", createSupplier);
-app.put("/:id", updateSupplier);
-app.delete("/:id", deleteSupplier);
+
+// Protected: loggedin user and admin can view suppliers
+app.get("/", authenticate, authorizeRoles("admin", "user"), getAllSuppliers);
+app.get("/:id", authenticate, authorizeRoles("admin", "user"), getSupplierById);
+app.post("/", authenticate, authorizeRoles("admin", "user"), createSupplier); // Protected: loggedin user and admin can create
+app.put("/:id", authenticate, authorizeRoles("admin", "user"), updateSupplier);// Protected: loggedin user and admin can update
+app.delete("/:id", authenticate, authorizeRoles("admin", "user"), deleteSupplier);// Protected: loggedin user and admin can delete
 
 module.exports = app;
