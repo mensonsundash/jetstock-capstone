9293a16ff2e0b9f32eb751750bb4a02ecfb3a307
menso431 <menson.sundash@gmail.com>
2026-03-28 09:09:19 +1100

    Dashboard Layout: topbar with logout context

---
+++ client/src/components/layout/Topbar.jsx
@@ -0,0 +1,34 @@
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import { useAuth } from "../../hooks/useAuth";
+
+const Topbar = ({ drawerWidth }) => {
+    //logout function from auth context
+  const { logout } = useAuth();
+
+  return (
+    // fixed topbar
+    <AppBar
+      position="fixed"
+      sx={{
+        width: `calc(100% - ${drawerWidth}px)`,
+        ml: `${drawerWidth}px`,
+      }}
+    >
+        {/* headers */}
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h6" noWrap component="div">
+          JetStock
+        </Typography>
+        
+        {/* logout button */}
+        <Box>
+          <Button color="inherit" onClick={logout}>
+            Logout
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Topbar;
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,8 +5,19 @@ export const AuthProvider = ({children}) => {
     // store logged in user
     const [user, setUser] = useState(null);
     
+    // reset user details to null when logout
+    const logout = () => {
+        setUser(null);
+    }
+
     return(
-        <AuthContext.Provider value={{ user, setUser, isAuthenticated: !!user}}>
+        <AuthContext.Provider value={{ 
+            user, 
+            setUser,
+            logout,
+            isAuthenticated: !!user
+        }}>
+            
             {children}
         </AuthContext.Provider>
     );
