2fd05d6489087f167c0c1c1e434afb39f9345721
menso431 <menson.sundash@gmail.com>
2026-03-28 23:43:32 +1100

    Frontend-categories: category api helper for CRUD operations

--- client/src/api/categoryApi.js
+++ client/src/api/categoryApi.js
@@ -5,3 +5,21 @@ export const getAllCategories = async () => {
   const response = await axiosClient.get("/categories");
   return response.data;
 };
+
+// Create a new Category
+export const createCategory = async (payload) => {
+  const response = await axiosClient.post("/categories", payload);
+  return response.data;
+}
+
+//update category by id
+export const updateCategory = async (id, payload) => {
+  const response = await axiosClient.put(`/categories/${id}`, payload);
+  return response.data;
+}
+
+// Delete category by id
+export const deleteCategory = async (id) => {
+  const response = await axiosClient.delete(`/categories/${id}`);
+  return response.data;
+}
