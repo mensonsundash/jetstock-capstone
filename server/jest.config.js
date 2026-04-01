12f1bdc626a368ea13d273042cc966d7114166ea
menso431 <menson.sundash@gmail.com>
2026-04-01 21:51:11 +1100

    testing-setup: jet backend config and testing source

---
+++ server/jest.config.js
@@ -0,0 +1,6 @@
+// jest config to run test in node.js environment
+module.exports = {
+    testEnvironment: "node",
+    clearMocks: true, //resetting call history every test
+    verbose: true // showing test output detailed in terminal
+}
